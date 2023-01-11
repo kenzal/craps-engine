c730fc457d5505a03a4a0adafa771a07efc1b6ce
Kenzal Hunter <kenzal.hunter@gmail.com>
2023-01-10 21:02:35 -0500

    Completed Win/Lose conditons for bets, more unit tests

--- craps/table/bet.py
+++ craps/table/bet.py
@@ -1,10 +1,20 @@
 from craps.table.puck import Puck
 from craps.dice import Outcome as DiceOutcome
+from enum import Enum
 import craps.table.config as TableConfig
 import typing
 
 
+class BetStatus(Enum):
+    ON = 'ON'
+    OFF = 'OFF'
+
+
 class InvalidBet(Exception):
+    pass
+
+
+class BadBetAction(Exception):
     pass
 
 
@@ -13,12 +23,13 @@ class Bet:
     odds: int = None
     _puck: Puck
     _table_config: TableConfig
-    location: typing.Union[int, None]
+    location: typing.Union[int, None] = None
     allow_odds: bool = False
     has_vig: bool = False
     can_toggle: bool = False
     single_roll: bool = False
     multi_bet: int = 0
+    _override_toggle: typing.Union[BetStatus, None] = None
 
     def __init__(self, wager: int, puck: Puck, table_config: TableConfig, odds: int = None, location=None):
         if wager <= 0:
@@ -49,16 +60,50 @@ class Bet:
     def get_payout(self, outcome: DiceOutcome) -> int:
         pass
 
+    def is_on(self) -> bool:
+        if self._override_toggle == BetStatus.ON or not self.can_toggle:
+            return True
+        if self._override_toggle == BetStatus.OFF:
+            return False
+        return self._puck.is_on()
+
+    def turn_off(self):
+        if not self.can_toggle:
+            raise BadBetAction(f'Can not turn off {self.__class__.__name__} bet')
+        self._override_toggle = BetStatus.OFF
+
+    def turn_on(self):
+        self._override_toggle = BetStatus.ON
+
+    def follow_puck(self):
+        self._override_toggle = None
+
 
 class PassLine(Bet):
     allow_odds = True
 
     def move(self, location: int):
         if self.location:
-            raise InvalidBet('Can not move {} bet twice.'.format(self.__class__.__name__))
+            raise InvalidBet('Can not move {} bet after point.'.format(self.__class__.__name__))
         if location not in self._table_config.odds.valid_keys():
             raise InvalidBet('Illegal location for {} bet'.format(self.__class__.__name__))
         self.location = location
+
+    def is_loser(self, outcome: DiceOutcome) -> bool:
+        if self.location and outcome.total() == 7:
+            return True
+        if self.location is None and not self._table_config.is_crapless and outcome.total() in [2, 3, 12]:
+            return True
+        return False
+
+    def is_winner(self, outcome: DiceOutcome) -> bool:
+        if self.location and outcome.total() == self.location:
+            return True
+        if self.location is None and outcome.total() == 7:
+            return True
+        if self.location is None and not self._table_config.is_crapless and outcome.total() == 11:
+            return True
+        return False
 
 
 class Put(PassLine):
@@ -74,10 +119,26 @@ class Come(PassLine):
 
 
 class DontPass(PassLine):
-    pass
+    def _check_valid(self):
+        if self._table_config.is_crapless:
+            raise InvalidBet(f'{self.__class__.__name__} is not a valid bet for Crapless')
+
+    def is_winner(self, outcome: DiceOutcome) -> bool:
+        if self.location and outcome.total() == 7:
+            return True
+        if self.location is None and outcome.total() in [2, 3, 12] and outcome.total() != self._table_config.dont_bar:
+            return True
+        return False
+
+    def is_loser(self, outcome: DiceOutcome) -> bool:
+        if self.location and outcome.total() == self.location:
+            return True
+        if self.location is None and outcome.total() in [7, 11]:
+            return True
+        return False
 
 
-class DontCome(Come, DontPass):
+class DontCome(DontPass):
     pass
 
 
@@ -90,6 +151,12 @@ class Field(Bet):
 
 class Place(Bet):
     can_toggle = True
+
+    def is_loser(self, outcome: DiceOutcome):
+        return self.is_on() and outcome.total() == 7
+
+    def is_winner(self, outcome: DiceOutcome) -> bool:
+        return self.is_on() and outcome.total() == self.location
 
     def _check_valid(self):
         if not self.location:
@@ -112,10 +179,10 @@ class HardWay(Bet):
             raise InvalidBet('{1} is not a valid location for a {0} bet'.format(self.__class__.__name__, self.location))
 
     def is_winner(self, outcome: DiceOutcome):
-        return outcome.total() == self.location and outcome.is_hard()
+        return self.is_on() and outcome.total() == self.location and outcome.is_hard()
 
     def is_loser(self, outcome: DiceOutcome):
-        return outcome.total() == self.location and not outcome.is_hard()
+        return self.is_on() and outcome.total() == self.location and not outcome.is_hard()
 
 
 class Prop(Bet):
@@ -154,9 +221,21 @@ class Horn(Prop):
 class HornHigh(Horn):
     multi_bet = 5
 
+    def _check_valid(self):
+        if not self.location:
+            raise InvalidBet('{0} bet requires a location'.format(self.__class__.__name__))
+        if self.location not in [2, 3, 11, 12]:
+            raise InvalidBet('{1} is not a valid location for a {0} bet'.format(self.__class__.__name__, self.location))
+
 
 class World(Prop):
     multi_bet = 5
+
+    def is_winner(self, outcome: DiceOutcome):
+        return outcome.total() in [2, 3, 11, 12]
+
+    def is_loser(self, outcome: DiceOutcome):
+        return outcome.total() not in [2, 3, 7, 11, 12]
 
 
 class Craps3Way(Prop):
@@ -175,4 +254,17 @@ class CnE(Prop):
 
 class Lay(Bet):
     has_vig = True
+    _override_toggle = BetStatus.ON
     can_toggle = True
+
+    def is_winner(self, outcome: DiceOutcome):
+        return self.is_on() and outcome.total() == 7
+
+    def is_loser(self, outcome: DiceOutcome) -> bool:
+        return self.is_on() and outcome.total() == self.location
+
+    def _check_valid(self):
+        if not self.location:
+            raise InvalidBet('{0} bet requires a location'.format(self.__class__.__name__))
+        if self.location not in self._table_config.odds.valid_keys():
+            raise InvalidBet('{1} is not a valid location for a {0} bet'.format(self.__class__.__name__, self.location))
--- tests/craps/table/test_bet.py
+++ tests/craps/table/test_bet.py
@@ -1,6 +1,7 @@
 import unittest
 import craps.table as table
 import craps.table.config as TableConfig
+from craps.dice import Outcome
 
 
 class TestBet(unittest.TestCase):
@@ -12,6 +13,19 @@ class TestBet(unittest.TestCase):
         self.table_config = table.config.Config()
         self.puck = table.puck.Puck(self.table_config)
         self.wager = 50
+        self.outcomes = {
+            2:  Outcome(1, 1),  # Aces
+            3:  Outcome(1, 2),  # Ace-Deuce, Shocker
+            4:  Outcome(2, 2),  # Little Joe
+            5:  Outcome(2, 3),  # Fiver
+            6:  Outcome(3, 3),  #
+            7:  Outcome(3, 4),  # Big Red
+            8:  Outcome(4, 4),  #
+            9:  Outcome(4, 5),  # Niner
+            10: Outcome(5, 5),  # The Big one on the End
+            11: Outcome(5, 6),  # Yo
+            12: Outcome(6, 6),  # Midnight, Boxcars
+        }
 
     def test_pass_line(self):
         bet = table.bet.PassLine(self.wager, puck=self.puck, table_config=self.table_config)
@@ -22,9 +36,37 @@ class TestBet(unittest.TestCase):
         self.assertFalse(bet.single_roll)
         self.assertEqual(bet.wager, self.wager)
         self.assertIsNone(bet.odds)
+        self.assertTrue(bet.is_winner(self.outcomes[7]), 'Wins on Seven')
+        self.assertTrue(bet.is_winner(self.outcomes[11]), 'Wins on Yo')
+        self.assertTrue(bet.is_loser(self.outcomes[2]), 'Loses on Aces')
+        self.assertTrue(bet.is_loser(self.outcomes[3]), 'Loses on Three')
+        self.assertTrue(bet.is_loser(self.outcomes[12]), 'Loses on Midnight')
+
+        point_roll = self.outcomes[4]
+        other_point = self.outcomes[6]
+        self.assertFalse(bet.is_winner(point_roll), 'Does Not Win on point number')
+        self.assertFalse(bet.is_loser(point_roll), 'Does Not Lose on point number')
+
+        bet.move(point_roll.total())
+        with self.assertRaises(table.bet.InvalidBet):  # Can not move twice
+            bet.move(other_point.total())
+
+        self.assertFalse(bet.is_winner(self.outcomes[7]), 'Does not win on Seven')
+        self.assertTrue(bet.is_loser(self.outcomes[7]), 'Loses on Seven')
+        self.assertTrue(bet.is_winner(self.outcomes[4]), 'Wins on Point')
+        self.assertFalse(bet.is_loser(self.outcomes[4]), 'Does not lose on Point')
+        self.assertFalse(bet.is_winner(self.outcomes[11]), 'Does not win on Yo')
+        self.assertFalse(bet.is_loser(self.outcomes[2]), 'Does not lose on Aces')
+        self.assertFalse(bet.is_loser(self.outcomes[3]), 'Does not lose on Three')
+        self.assertFalse(bet.is_loser(self.outcomes[12]), 'Does not lose on Midnight')
+        self.assertFalse(bet.is_winner(other_point), 'Does not win on other point')
+        self.assertFalse(bet.is_loser(other_point), 'Does not lose on other point')
 
     def test_put(self):
-        bet = table.bet.Put(self.wager, puck=self.puck, table_config=self.table_config, location=4)
+        point_roll = self.outcomes[4]
+        other_point = self.outcomes[6]
+        bet = table.bet.Put(self.wager, puck=self.puck, table_config=self.table_config,
+                            location=self.outcomes[4].total())
         self.assertTrue(bet.allow_odds)
         self.assertFalse(bet.can_toggle)
         self.assertFalse(bet.has_vig)
@@ -32,6 +74,20 @@ class TestBet(unittest.TestCase):
         self.assertFalse(bet.single_roll)
         self.assertEqual(bet.wager, self.wager)
         self.assertIsNone(bet.odds)
+
+        with self.assertRaises(table.bet.InvalidBet):  # Can not move twice
+            bet.move(other_point.total())
+
+        self.assertFalse(bet.is_winner(self.outcomes[7]), 'Does not win on Seven')
+        self.assertTrue(bet.is_loser(self.outcomes[7]), 'Loses on Seven')
+        self.assertTrue(bet.is_winner(self.outcomes[4]), 'Wins on Point')
+        self.assertFalse(bet.is_loser(self.outcomes[4]), 'Does not lose on Point')
+        self.assertFalse(bet.is_winner(self.outcomes[11]), 'Does not win on Yo')
+        self.assertFalse(bet.is_loser(self.outcomes[2]), 'Does not lose on Aces')
+        self.assertFalse(bet.is_loser(self.outcomes[3]), 'Does not lose on Three')
+        self.assertFalse(bet.is_loser(self.outcomes[12]), 'Does not lose on Midnight')
+        self.assertFalse(bet.is_winner(other_point), 'Does not win on other point')
+        self.assertFalse(bet.is_loser(other_point), 'Does not lose on other point')
 
     def test_come(self):
         bet = table.bet.Come(self.wager, puck=self.puck, table_config=self.table_config)
@@ -42,6 +98,38 @@ class TestBet(unittest.TestCase):
         self.assertFalse(bet.single_roll)
         self.assertEqual(bet.wager, self.wager)
         self.assertIsNone(bet.odds)
+        self.assertTrue(bet.allow_odds)
+        self.assertFalse(bet.can_toggle)
+        self.assertFalse(bet.has_vig)
+        self.assertFalse(bet.multi_bet)
+        self.assertFalse(bet.single_roll)
+        self.assertEqual(bet.wager, self.wager)
+        self.assertIsNone(bet.odds)
+        self.assertTrue(bet.is_winner(self.outcomes[7]), 'Wins on Seven')
+        self.assertTrue(bet.is_winner(self.outcomes[11]), 'Wins on Yo')
+        self.assertTrue(bet.is_loser(self.outcomes[2]), 'Loses on Aces')
+        self.assertTrue(bet.is_loser(self.outcomes[3]), 'Loses on Three')
+        self.assertTrue(bet.is_loser(self.outcomes[12]), 'Loses on Midnight')
+
+        point_roll = self.outcomes[4]
+        other_point = self.outcomes[6]
+        self.assertFalse(bet.is_winner(point_roll), 'Does Not Win on point number')
+        self.assertFalse(bet.is_loser(point_roll), 'Does Not Lose on point number')
+
+        bet.move(point_roll.total())
+        with self.assertRaises(table.bet.InvalidBet):  # Can not move twice
+            bet.move(other_point.total())
+
+        self.assertFalse(bet.is_winner(self.outcomes[7]), 'Does not win on Seven')
+        self.assertTrue(bet.is_loser(self.outcomes[7]), 'Loses on Seven')
+        self.assertTrue(bet.is_winner(self.outcomes[4]), 'Wins on Point')
+        self.assertFalse(bet.is_loser(self.outcomes[4]), 'Does not lose on Point')
+        self.assertFalse(bet.is_winner(self.outcomes[11]), 'Does not win on Yo')
+        self.assertFalse(bet.is_loser(self.outcomes[2]), 'Does not lose on Aces')
+        self.assertFalse(bet.is_loser(self.outcomes[3]), 'Does not lose on Three')
+        self.assertFalse(bet.is_loser(self.outcomes[12]), 'Does not lose on Midnight')
+        self.assertFalse(bet.is_winner(other_point), 'Does not win on other point')
+        self.assertFalse(bet.is_loser(other_point), 'Does not lose on other point')
 
     def test_dont_pass(self):
         bet = table.bet.DontPass(self.wager, puck=self.puck, table_config=self.table_config)
@@ -53,6 +141,33 @@ class TestBet(unittest.TestCase):
         self.assertEqual(bet.wager, self.wager)
         self.assertIsNone(bet.odds)
 
+        self.assertTrue(bet.is_loser(self.outcomes[7]), 'Loses on Seven')
+        self.assertTrue(bet.is_loser(self.outcomes[11]), 'Wins on Yo')
+        self.assertTrue(bet.is_winner(self.outcomes[2]), 'Wins on Aces')
+        self.assertTrue(bet.is_winner(self.outcomes[3]), 'Wins on Three')
+        self.assertFalse(bet.is_winner(self.outcomes[12]), 'Does not lose on Midnight')
+        self.assertFalse(bet.is_loser(self.outcomes[12]), 'Does not win on Midnight')
+
+        point_roll = self.outcomes[4]
+        other_point = self.outcomes[6]
+        self.assertFalse(bet.is_winner(point_roll), 'Does Not Win on point number')
+        self.assertFalse(bet.is_loser(point_roll), 'Does Not Lose on point number')
+
+        bet.move(point_roll.total())
+        with self.assertRaises(table.bet.InvalidBet):  # Can not move twice
+            bet.move(other_point.total())
+
+        self.assertTrue(bet.is_winner(self.outcomes[7]), 'Wins on Seven')
+        self.assertFalse(bet.is_loser(self.outcomes[7]), 'Does not lose on Seven')
+        self.assertTrue(bet.is_loser(self.outcomes[4]), 'Loses on Point')
+        self.assertFalse(bet.is_winner(self.outcomes[4]), 'Does not win on Point')
+        self.assertFalse(bet.is_loser(self.outcomes[11]), 'Does not lose on Yo')
+        self.assertFalse(bet.is_winner(self.outcomes[2]), 'Does not win on Aces')
+        self.assertFalse(bet.is_winner(self.outcomes[3]), 'Does not win on Three')
+        self.assertFalse(bet.is_winner(self.outcomes[12]), 'Does not win on Midnight')
+        self.assertFalse(bet.is_winner(other_point), 'Does not win on other point')
+        self.assertFalse(bet.is_loser(other_point), 'Does not lose on other point')
+
     def test_dont_come(self):
         bet = table.bet.DontCome(self.wager, puck=self.puck, table_config=self.table_config)
         self.assertTrue(bet.allow_odds)
@@ -62,6 +177,26 @@ class TestBet(unittest.TestCase):
         self.assertFalse(bet.single_roll)
         self.assertEqual(bet.wager, self.wager)
         self.assertIsNone(bet.odds)
+
+        point_roll = self.outcomes[4]
+        other_point = self.outcomes[6]
+        self.assertFalse(bet.is_winner(point_roll), 'Does Not Win on point number')
+        self.assertFalse(bet.is_loser(point_roll), 'Does Not Lose on point number')
+
+        bet.move(point_roll.total())
+        with self.assertRaises(table.bet.InvalidBet):  # Can not move twice
+            bet.move(other_point.total())
+
+        self.assertTrue(bet.is_winner(self.outcomes[7]), 'Wins on Seven')
+        self.assertFalse(bet.is_loser(self.outcomes[7]), 'Does not lose on Seven')
+        self.assertTrue(bet.is_loser(self.outcomes[4]), 'Loses on Point')
+        self.assertFalse(bet.is_winner(self.outcomes[4]), 'Does not win on Point')
+        self.assertFalse(bet.is_loser(self.outcomes[11]), 'Does not lose on Yo')
+        self.assertFalse(bet.is_winner(self.outcomes[2]), 'Does not win on Aces')
+        self.assertFalse(bet.is_winner(self.outcomes[3]), 'Does not win on Three')
+        self.assertFalse(bet.is_winner(self.outcomes[12]), 'Does not win on Midnight')
+        self.assertFalse(bet.is_winner(other_point), 'Does not win on other point')
+        self.assertFalse(bet.is_loser(other_point), 'Does not lose on other point')
 
     def test_field(self):
         bet = table.bet.Field(self.wager, puck=self.puck, table_config=self.table_config)
@@ -73,8 +208,21 @@ class TestBet(unittest.TestCase):
         self.assertEqual(bet.wager, self.wager)
         self.assertIsNone(bet.odds)
 
+        for outcome in self.outcomes.values():
+            with self.subTest(i=outcome.total()):
+                self.assertTrue(bet.is_winner(outcome)) if outcome.total() in [2, 3, 4, 9, 10, 11, 12] \
+                    else self.assertFalse(bet.is_winner(outcome))
+                self.assertTrue(bet.is_loser(outcome)) if outcome.total() in [5, 6, 7, 8] \
+                    else self.assertFalse(bet.is_loser(outcome))
+
     def test_place(self):
-        bet = table.bet.Place(self.wager, puck=self.puck, location=4, table_config=self.table_config)
+        place_outcome = self.outcomes[4]
+        other_outcome = self.outcomes[6]
+        seven_outcome = self.outcomes[7]
+        bet = table.bet.Place(self.wager,
+                              puck=self.puck,
+                              location=place_outcome.total(),
+                              table_config=self.table_config)
         self.assertFalse(bet.allow_odds)
         self.assertTrue(bet.can_toggle)
         self.assertFalse(bet.has_vig)
@@ -83,8 +231,49 @@ class TestBet(unittest.TestCase):
         self.assertEqual(bet.wager, self.wager)
         self.assertIsNone(bet.odds)
 
+        self.puck.place(other_outcome.total())  # Puck is On
+        self.assertTrue(bet.is_on())
+        self.assertTrue(bet.is_winner(place_outcome))
+        self.assertFalse(bet.is_loser(place_outcome))
+        self.assertFalse(bet.is_winner(other_outcome))
+        self.assertFalse(bet.is_loser(other_outcome))
+        self.assertFalse(bet.is_winner(seven_outcome))
+        self.assertTrue(bet.is_loser(seven_outcome))
+
+        bet.turn_off()  # Turn off bets (puck still on)
+
+        for outcome in self.outcomes.values():
+            with self.subTest("No effect when bet is off",
+                              i=outcome.total()):
+                self.assertFalse(bet.is_on())
+                self.assertFalse(bet.is_winner(outcome))
+                self.assertFalse(bet.is_loser(outcome))
+
+        bet.follow_puck()  # Bet again following puck
+        self.puck.remove()  # Puck is off
+
+        for outcome in self.outcomes.values():
+            with self.subTest("No effect when puck is off and bet follows puck",
+                              i=outcome.total()):
+                self.assertFalse(bet.is_on())
+                self.assertFalse(bet.is_winner(outcome))
+                self.assertFalse(bet.is_loser(outcome))
+
+        bet.turn_on()  # Bet is on though puck is off
+        self.assertTrue(bet.is_on())
+        self.assertTrue(self.puck.is_off())
+        self.assertTrue(bet.is_winner(place_outcome))
+        self.assertFalse(bet.is_loser(place_outcome))
+        self.assertFalse(bet.is_winner(other_outcome))
+        self.assertFalse(bet.is_loser(other_outcome))
+        self.assertFalse(bet.is_winner(seven_outcome))
+        self.assertTrue(bet.is_loser(seven_outcome))
+
     def test_buy(self):
-        bet = table.bet.Buy(self.wager, puck=self.puck, location=4, table_config=self.table_config)
+        place_outcome = self.outcomes[4]
+        other_outcome = self.outcomes[6]
+        seven_outcome = self.outcomes[7]
+        bet = table.bet.Buy(self.wager, puck=self.puck, location=place_outcome.total(), table_config=self.table_config)
         self.assertFalse(bet.allow_odds)
         self.assertTrue(bet.can_toggle)
         self.assertTrue(bet.has_vig)
@@ -93,8 +282,52 @@ class TestBet(unittest.TestCase):
         self.assertEqual(bet.wager, self.wager)
         self.assertIsNone(bet.odds)
 
+        self.puck.place(other_outcome.total())  # Puck is On
+        self.assertTrue(bet.is_on())
+        self.assertTrue(bet.is_winner(place_outcome))
+        self.assertFalse(bet.is_loser(place_outcome))
+        self.assertFalse(bet.is_winner(other_outcome))
+        self.assertFalse(bet.is_loser(other_outcome))
+        self.assertFalse(bet.is_winner(seven_outcome))
+        self.assertTrue(bet.is_loser(seven_outcome))
+
+        bet.turn_off()  # Turn off bets (puck still on)
+
+        for outcome in self.outcomes.values():
+            with self.subTest("No effect when bet is off",
+                              i=outcome.total()):
+                self.assertFalse(bet.is_on())
+                self.assertFalse(bet.is_winner(outcome))
+                self.assertFalse(bet.is_loser(outcome))
+
+        bet.follow_puck()  # Bet again following puck
+        self.puck.remove()  # Puck is off
+
+        for outcome in self.outcomes.values():
+            with self.subTest("No effect when puck is off and bet follows puck",
+                              i=outcome.total()):
+                self.assertFalse(bet.is_on())
+                self.assertFalse(bet.is_winner(outcome))
+                self.assertFalse(bet.is_loser(outcome))
+
+        bet.turn_on()  # Bet is on though puck is off
+        self.assertTrue(bet.is_on())
+        self.assertTrue(self.puck.is_off())
+        self.assertTrue(bet.is_winner(place_outcome))
+        self.assertFalse(bet.is_loser(place_outcome))
+        self.assertFalse(bet.is_winner(other_outcome))
+        self.assertFalse(bet.is_loser(other_outcome))
+        self.assertFalse(bet.is_winner(seven_outcome))
+        self.assertTrue(bet.is_loser(seven_outcome))
+
     def test_lay(self):
-        bet = table.bet.Lay(self.wager, puck=self.puck, location=6, table_config=self.table_config)
+        place_outcome = self.outcomes[4]
+        other_outcome = self.outcomes[6]
+        seven_outcome = self.outcomes[7]
+        bet = table.bet.Lay(self.wager,
+                            puck=self.puck,
+                            location=place_outcome.total(),
+                            table_config=self.table_config)
         self.assertFalse(bet.allow_odds)
         self.assertTrue(bet.can_toggle)
         self.assertTrue(bet.has_vig)
@@ -102,6 +335,23 @@ class TestBet(unittest.TestCase):
         self.assertFalse(bet.single_roll)
         self.assertEqual(bet.wager, self.wager)
         self.assertIsNone(bet.odds)
+
+        self.assertTrue(bet.is_on())
+        self.assertTrue(bet.is_winner(seven_outcome))
+        self.assertFalse(bet.is_loser(seven_outcome))
+        self.assertFalse(bet.is_winner(other_outcome))
+        self.assertFalse(bet.is_loser(other_outcome))
+        self.assertFalse(bet.is_winner(place_outcome))
+        self.assertTrue(bet.is_loser(place_outcome))
+
+        bet.turn_off()  # Turn off bets (puck still on)
+
+        for outcome in self.outcomes.values():
+            with self.subTest("No effect when bet is off",
+                              i=outcome.total()):
+                self.assertFalse(bet.is_on())
+                self.assertFalse(bet.is_winner(outcome))
+                self.assertFalse(bet.is_loser(outcome))
 
 
 if __name__ == '__main__':
